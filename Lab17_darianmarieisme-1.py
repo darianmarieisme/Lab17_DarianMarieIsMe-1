48e5ff11a58afd67415ba763d14b59713c4627e7
Darian Bruce <darianmariebruce@gmail.com>
2026-05-15 13:35:13 -0400

    refactoring

--- Lab17_darianmarieisme-1.py
+++ Lab17_darianmarieisme-1.py
@@ -8,19 +8,24 @@ from operator import itemgetter
 
 import requests
 
-# Make an API call and check the response.
-url = "https://hacker-news.firebaseio.com/v0/topstories.json"
-r = requests.get(url)
-print(f"Status code: {r.status_code}")
+def get_top_story_ids() -> list[int]:
+    '''get the the ids for the current top stories on hacker news
+    '''
+    url: str = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    r = requests.get(url)
+    print(f"Status code: {r.status_code}")
 
-# Process information about each submission.
-submission_ids = r.json()
-submission_dicts = []
-for submission_id in submission_ids[:30]:
-    # Make a new API call for each submission.
+    return response.json()
+
+def get_submission_dict(submission_id: int) -> dict[str, str | int]:
+    '''Build and return a dictionary containing data for one Hacker 
+    News article'''
+
+
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
+
     response_dict = r.json()
 
     try:
@@ -28,16 +33,25 @@ for submission_id in submission_ids[:30]:
     except KeyError:
         comments = 0
 
-    # Build a dictionary for each article.
     submission_dict = {
         'title': response_dict['title'],
         'hn_link': f"https://news.ycombinator.com/item?id={submission_id}",
         'comments': comments,
     }
-    submission_dicts.append(submission_dict)
 
-submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
-                            reverse=True)
+    return submission_dict
+
+def get_submission_dicts(submission_ids: list[int]) -> list[dict[str, str | int]]:
+    '''Build and return a list of dictionaries for the first 30 Hacker
+    News articles.'''
+
+    submission_dicts = []
+
+    for submission_id in submission_ids[:30]:
+        submission_dict = get_submission_dict(submission_id)
+        submission_dicts.append(submission_dict)
+
+    return submission_dicts
 
 for submission_dict in submission_dicts:
     print(f"\nTitle: {submission_dict['title']}")
